d61e58fd2d0f2c499bfb3316700f807c8db0568f
kasshii28 <gp22a043@oecu.jp>
2024-11-17 13:02:23 +0900

    add mod

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import './styles/global.css'
+import '@/styles/global.css'
 import { Link } from '@tanstack/react-router'
 
 function App() {
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,8 @@ import { RouterProvider, createRouter } from '@tanstack/react-router'
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import './styles/global.css'
-
-// Import the generated route tree
-import { routeTree } from './routeTree.gen'
+import { routeTree } from './routeTree.gen' // Import the generated route tree
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 // Create a new router instance
 const router = createRouter({ routeTree })
@@ -16,13 +15,23 @@ declare module '@tanstack/react-router' {
   }
 }
 
+const queryClinet: QueryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
 // Render the app
 const rootElement = document.getElementById('root') as HTMLElement
 if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement)
   root.render(
     <StrictMode>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClinet}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </StrictMode>,
   )
 }
